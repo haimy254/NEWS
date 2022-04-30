d0b44d223ba12ed8ed7c87194beb9a78748c7ad8
haimy254 <haimanaare@gmail.com>
2022-04-30 11:59:42 +0300

    request initial commit

--- app/config.py
+++ app/config.py
@@ -1,2 +1,2 @@
 class Config:
-    NEWS_API_BASE_URL='https://newsapi.org/v2/top-headlines?country=kenya&apiKey=NEWS_API_KEY'
+    NEWS_API_BASE_URL='https://newsapi.org/v2/top-headlines?country=kenya&apiKey=API_KEY'
