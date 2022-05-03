72afa99d35b433648f99b1680d5d64539086c0a8
haimy254 <haimanaare@gmail.com>
2022-05-03 23:48:38 +0300

    added news category in views

--- app/views.py
+++ app/views.py
@@ -5,7 +5,15 @@ from app import app
 @app.route('/')
 def index():
     
-    popular_news= get_news('popular')
-    print(popular_news)
+    business_news= get_news('business')
+    print(business_news)
     title = 'home - Welcome to current world wide news website online'
-    return render_template('index.html', title = title,popular = popular_news)
+    
+    business_news= get_news('business')
+    entertainment_news= get_news('entertainment')
+    general_news= get_news('general')
+    health_news= get_news('health')
+    science_news=get_news('science')
+    technology_news= get_news('technology')
+    sport_news= get_news('sport')
+    return render_template('index.html', title = title,business = business_news, entertainment= entertainment_news, general= general_news, health = health_news, science= science_news, technology = technology_news, sport= sport_news)
