8290d5986e0342b522b2fc19f044bab4e57ff4bd
haimy254 <haimanaare@gmail.com>
2022-05-04 02:50:11 +0300

    updates

--- app/model/__init__.py
+++ app/model/__init__.py
@@ -1,2 +1,13 @@
+from ensurepip import bootstrap
+from flask import Flask
+from app.config import DevConfig
+from flask_bootstrap import Bootstrap
 
+app=Flask(__name__,instance_relative_config = True)
+app.config.from_object(DevConfig) 
+app.config.from_pyfile('config.py')
+
+bootstrap =Bootstrap(app)
+
+from app import views
 
--- app/views.py
+++ app/views.py
@@ -4,19 +4,9 @@ from .request import get_news
 from app import app 
 from .request import get_news,get_news
 
-@app.route('/')
-def index():
+@app.route('/news/<int:id>')
+def index(id):
     news = get_news(id)
     title = f'{news.title}'
-    # business_news= get_news('business')
-    # print(business_news)
-    # title = 'home - Welcome to current world wide news website online'
-    
-    # business_news= get_news('business')
-    # entertainment_news= get_news('entertainment')
-    # general_news= get_news('general')
-    # health_news= get_news('health')
-    # science_news=get_news('science')
-    # technology_news= get_news('technology')
-    # sport_news= get_news('sport')
+  
     return render_template('index.html', title = title,news = news )
