9176edcc06fdd82d3ae504be98045fda4dc77668
haimy254 <haimanaare@gmail.com>
2022-05-03 23:20:00 +0300

    second commit views

--- app/views.py
+++ app/views.py
@@ -1,7 +1,11 @@
 from flask import render_template
-
+from .request import get_news
 from app import app 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    
+    popular_news= get_news('popular')
+    print(popular_news)
+    title = 'home - Welcome to current world wide news website online'
+    return render_template('index.html', title = title,popular = popular_news)
