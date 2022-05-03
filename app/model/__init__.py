8b078985f39ae0f71ca6bd345e93465fb6a5f8b3
haimy254 <haimanaare@gmail.com>
2022-05-03 12:50:14 +0300

    first test init

--- app/model/__init__.py
+++ app/model/__init__.py
@@ -1,11 +1,2 @@
-# from tkinter import Image
-# from turtle import title
-
-# from app.model.news_test import News
 
 
-# 1. Title -The  title of the news
-# 2. Auhtor - The writer of the News
-# 3. Description - The brief description of the news 
-# 4. PublishedAt - The time it was published 
-# 5. Image - The image of the news
--- app/news_test.py
+++ app/news_test.py
@@ -5,7 +5,14 @@ News = news.News
 
 class NewsTest(unittest.TestCases):
     def setUp(self):
-        self.new_news = ('America goes down','John Wit','America goes to war with rassia','bbc-news','http//bbc news.com')
+        self.new_news = ('America goes down','John Wit','America goes to war with russia','bbc-news','http//bbc news.com')
+        
+    def test__init__(self):
+        self.assertEqual(self.new_news.title,"America goes down")
+        self.assertEqual(self.new_news.author,"John Wit")
+        self.assertEqual(self.new_news.description,"America goes to war with russia")
+        self.assertEqual(self.new_news.publishedAt,"bbd-news")
+        self.assertEqual(self.new_news.image,"http//bbc news.com")
         
     def test_instance(self):
          self.assertTrue(isinstance(self.new_news,News))
