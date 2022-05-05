a75fc8db8c59e6d3de5a515bb832414c5322f032
haimy254 <haimanaare@gmail.com>
2022-05-05 03:13:53 +0300

    config changed

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 class Config:
     NEWS_API_BASE_URL='https://newsapi.org/v2/everything?q=Apple&from=2022-05-03&sortBy=popularity&apiKey=API_KEY'
-    
+    NEWS_API_KEY='25a14688847949f39f86a855aa918ad8'
 class ProdConfig(Config):
     pass
 
