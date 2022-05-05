c3a60eb63336a3340699b651f44795c1061ab805
haimy254 <haimanaare@gmail.com>
2022-05-05 06:30:46 +0300

    review

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,11 @@
 
+from turtle import title
 from flask import render_template
 from ..request import get_news
 from app import app 
 from request import get_news,get_news, search_news
+from flask import render_template,request,redirect,url_for
+
 
 
 @app.route('/news/<int:id>')
@@ -18,3 +21,23 @@ def search(news_name):
     searched_news= search_news(news_name_format)
     title = f'search results for {news_name}'
     return render_template('search.html',news= searched_news)
+
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    # Getting popular movie
+    business_news = get_news('business')
+    entertainment_news= get_news('entertainment')
+    general_news = get_news('general')
+    
+    title = 'News headlines'
+    search_news = request.args.get('news_query')
+
+    if search_news:
+        return redirect(url_for('search',movie_name=search_news))
+    else:
+        return render_template('index.html', title = title, business =  business_news, entertainment  = entertainment_news, general =  general_news )
